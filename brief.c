fbb9cf7870ce093ff7b4aea869d9da40cd86f3f9
unknown <khalidhagane62@gmail.com>
2021-11-22 23:29:00 +0100

    termine case 2, commences case 3

--- brief.c
+++ brief.c
@@ -23,6 +23,7 @@ int main(){
 
     int choix;
     int choix2;
+    int choix3;
 
     affiche_menu();
 
@@ -47,8 +48,50 @@ int main(){
                  scanf("%s",compte[i].cin);
                  printf("entrer le montant : ");
                  scanf("%f",&compte[i].montant);
+
+                 printf("entrer 1 pour saisir autre personne ou 2 pour retourner au menu principale :");
+                 scanf("%d",&choix2);
+
+                 while(choix2 != 2){
+
+                    if(choix2 == 1){
+                         printf("entrer le nom : ");
+                         scanf("%s",compte[i].nom);
+                         printf("entrer le prenom : ");
+                         scanf("%s",compte[i].prenom);
+                         printf("entrer le CIN : ");
+                         scanf("%s",compte[i].cin);
+                         printf("entrer le montant : ");
+                         scanf("%f",&compte[i].montant);
+                       }
+
+                     else {
+                            printf("L'option n'existe pas \n");
+                          }
+                           printf("entrer 1 pour saisir autre personne ou 2 pour retourner au menu principale :");
+                           scanf("%d",&choix2);
+                     }
                  break;
-        case 3 : printf("");
+        case 3 :
+              //  printf("Entrer 1 pour Retrait ou 2 pour Depot ou 3 pour annuler :");
+                //scanf("%d", &choix3);
+                   // while (choix3 != 3){
+                       // Retrait
+                     //  if (choix3 == 1) {
+                        //    printf("Entrer cin : ");
+                        //    scanf("" );
+                     //  }
+                       // montant
+
+
+
+                       // cas erreur // non trouve
+
+                     //  }
+                     // }
+                     // Depot
+
+
         case 4 : printf("");
         case 5 : printf("");
         case 6 : printf("");
